8322d4ea079aba94b4dcf5c8d4b43eed0326705c
= <=>
2015-11-25 00:05:56 -0800

    power

---
+++ py3/power.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+def power(x,n):
+    s = 1
+    while n > 0:
+        n = n - 1
+        s = s * x
+        print(s)
+        print(s)
+print(power(2,2))
+
